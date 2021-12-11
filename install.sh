e0460ed4f109e6b7ac1a4c48e2fd7f2fb140c676
Johannes Heesterman <jheesterman@outlook.com>
2021-12-11 14:40:32 +0100

    gogh terminal styling

--- install.sh
+++ install.sh
@@ -11,6 +11,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs
 
+export TERMINAL=gnome-terminal
+git clone https://github.com/Mayccoll/Gogh.git gogh
+./gogh/themes/gruvbox-dark.sh
+
 mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
